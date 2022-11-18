750591a5475b83fa5bb7cabdcc47ec1963e7e58c
Cheenu Khurana <cheenu.ck@gmail.com>
2022-11-18 22:40:22 +0530

    homepage ui

--- frontend/components/allMovies.js
+++ frontend/components/allMovies.js
@@ -3,6 +3,7 @@ import { MOVIES_NFT_CONTRACT_ADDRESS } from '../utils/constants';
 import { MOVIES_CONTRACT_ABI } from '../utils/contract_abis'
 import { useEffect } from "react"
 import { moviesData } from '../moviesData';
+import Link from 'next/link';
 
 const AllMovies = () => {
 
@@ -18,7 +19,7 @@ const AllMovies = () => {
 
     useEffect(() => {
 
-    },[])
+    }, [])
 
     const handleGetAllMovies = async () => {
         moviesContract.getAllMovies({
@@ -30,7 +31,19 @@ const AllMovies = () => {
 
     return (
         <div>
-            <button className="block mt-4 px-4 py-2 bg-pink-400" onClick={handleGetAllMovies}>Get ALL Movie NFTs</button>
+            {/* <button className="block px-4 py-2 bg-pink-400 mb-4" onClick={handleGetAllMovies}>Get ALL Movie NFTs</button> */}
+            <div className="flex flex-wrap gap-10 cursor-pointer justify-around">
+                {moviesData.map((movie) => (
+                    <Link href={`/${movie.name}`}>
+                        <div className="text-center">
+                            <img src={`/movies/${movie.name.replaceAll(' ', '_')}.jpeg`} alt="" className="border mx-auto w-48 h-72" />
+                            <p className="mt-2">{movie.name}</p>
+                            <p>{movie.release}</p>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+
         </div>
     )
 }
