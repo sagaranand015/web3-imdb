6a35050c40a39eb2044404b6b586d10365cd21d9
Sagar Anand <sagar.anand015@gmail.com>
2022-11-19 01:53:12 +0530

    updated with polygonid integration

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,9 +1,12 @@
+/* global BigInt */
 import React, { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
 import './App.css';
 import { FetchDataFromIpfsLink, UploadNftJson } from './nftStorage';
-import { DEMO_MOVIE_IMAGE, MOVIES_NFT_CONTRACT_ADDRESS } from './constants';
-import { MOVIES_CONTRACT_ABI } from './contract_abis'
+import { DEMO_MOVIE_IMAGE, MOVIES_NFT_CONTRACT_ADDRESS, RATERVERIFIER_CONTRACT_ADDRESS } from './constants';
+import { MOVIES_CONTRACT_ABI, RATERVERIFIER_CONTRACT_ABI } from './contract_abis';
+import { QRCodeSVG } from 'qrcode.react';
+import verificationQRCodeJson from './verificationQRCode.json';
 
 function App() {
 
@@ -20,6 +23,30 @@ function App() {
     MOVIES_CONTRACT_ABI,
     signer
   );
+
+  // reference to the raterVerifier contract
+  const raterVerifierContract = new ethers.Contract(
+    RATERVERIFIER_CONTRACT_ADDRESS,
+    RATERVERIFIER_CONTRACT_ABI,
+    signer
+  );
+
+  function hexToBytes(hex) {
+    for (var bytes = [], c = 0; c < hex.length; c += 2)
+      bytes.push(parseInt(hex.substr(c, 2), 16));
+    return bytes;
+  }
+
+  function fromLittleEndian(bytes) {
+    const n256 = BigInt(256);
+    let result = BigInt(0);
+    let base = BigInt(1);
+    bytes.forEach((byte) => {
+      result += base * BigInt(byte);
+      base = base * n256;
+    });
+    return result;
+  }
 
   const connectWalletHandler = async () => {
     const { ethereum } = window;
@@ -134,6 +161,34 @@ function App() {
     });
   }
 
+  async function setRequestForVerification() {
+    const circuitId = "credentialAtomicQuerySig";
+    const validatorAddress = "0xb1e86C4c687B85520eF4fd2a0d14e81970a15aFB";
+    const schemaHash = "be159805ee0986bb6480b8c6cfd87743" // extracted from PID Platform
+    const schemaEnd = fromLittleEndian(hexToBytes(schemaHash))
+    const query = {
+      schema: ethers.BigNumber.from(schemaEnd),
+      slotIndex: 2,
+      operator: 2,
+      value: [20020101, ...new Array(63).fill(0).map(i => 0)],
+      circuitId,
+    };
+    // // add the address of the contract just deployed
+    // const ERC20VerifierAddress = "0x461b78FCf300CE4eC582B727212fB09a935fCfF9"
+    // // let erc20Verifier = await hre.ethers.getContractAt("ERC20Verifier", ERC20VerifierAddress)
+    await raterVerifierContract.setZKPRequest(
+      1,
+      validatorAddress,
+      query
+    ).then(function (resp) {
+      console.log("Request set response: ", resp);
+    });
+  }
+
+  async function showVerificationQRCode() {
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -146,6 +201,13 @@ function App() {
         {/* <button onClick={() => getMovieRatingsByNumber(2)}>Get Movie Ratings with TokenId: 1</button> */}
         <button onClick={() => getUserRatings()}>Get Current User Ratings</button>
         <button onClick={() => getAvgForMovie(1)}>Get Average Rating of Movie: 1</button>
+        <button onClick={() => setRequestForVerification()}>Set Request for User Verification</button>
+        <button onClick={() => showVerificationQRCode()}>Show Verification QR Code</button>
+
+        <QRCodeSVG level="Q"
+          size="256"
+          value={JSON.stringify(verificationQRCodeJson)} />
+
       </header>
     </div >
   );
--- demo-app/src/contract_abis.js
+++ demo-app/src/contract_abis.js
@@ -573,3 +573,380 @@ export const MOVIES_CONTRACT_ABI = [
         "constant": true
     }
 ];
+
+export const RATERVERIFIER_CONTRACT_ABI = [
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "moviesNftContract",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "previousOwner",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "newOwner",
+                "type": "address"
+            }
+        ],
+        "name": "OwnershipTransferred",
+        "type": "event"
+    },
+    {
+        "inputs": [],
+        "name": "TRANSFER_REQUEST_ID",
+        "outputs": [
+            {
+                "internalType": "uint64",
+                "name": "",
+                "type": "uint64"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "name": "addressToId",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [],
+        "name": "getSupportedRequests",
+        "outputs": [
+            {
+                "internalType": "uint64[]",
+                "name": "arr",
+                "type": "uint64[]"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint64",
+                "name": "requestId",
+                "type": "uint64"
+            }
+        ],
+        "name": "getZKPRequest",
+        "outputs": [
+            {
+                "components": [
+                    {
+                        "internalType": "uint256",
+                        "name": "schema",
+                        "type": "uint256"
+                    },
+                    {
+                        "internalType": "uint256",
+                        "name": "slotIndex",
+                        "type": "uint256"
+                    },
+                    {
+                        "internalType": "uint256",
+                        "name": "operator",
+                        "type": "uint256"
+                    },
+                    {
+                        "internalType": "uint256[]",
+                        "name": "value",
+                        "type": "uint256[]"
+                    },
+                    {
+                        "internalType": "string",
+                        "name": "circuitId",
+                        "type": "string"
+                    }
+                ],
+                "internalType": "struct ICircuitValidator.CircuitQuery",
+                "name": "",
+                "type": "tuple"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "idToAddress",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            },
+            {
+                "internalType": "uint64",
+                "name": "",
+                "type": "uint64"
+            }
+        ],
+        "name": "proofs",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [],
+        "name": "renounceOwnership",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint64",
+                "name": "",
+                "type": "uint64"
+            }
+        ],
+        "name": "requestQueries",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "schema",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "slotIndex",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "operator",
+                "type": "uint256"
+            },
+            {
+                "internalType": "string",
+                "name": "circuitId",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint64",
+                "name": "",
+                "type": "uint64"
+            }
+        ],
+        "name": "requestValidators",
+        "outputs": [
+            {
+                "internalType": "contract ICircuitValidator",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint64",
+                "name": "requestId",
+                "type": "uint64"
+            },
+            {
+                "internalType": "contract ICircuitValidator",
+                "name": "validator",
+                "type": "address"
+            },
+            {
+                "components": [
+                    {
+                        "internalType": "uint256",
+                        "name": "schema",
+                        "type": "uint256"
+                    },
+                    {
+                        "internalType": "uint256",
+                        "name": "slotIndex",
+                        "type": "uint256"
+                    },
+                    {
+                        "internalType": "uint256",
+                        "name": "operator",
+                        "type": "uint256"
+                    },
+                    {
+                        "internalType": "uint256[]",
+                        "name": "value",
+                        "type": "uint256[]"
+                    },
+                    {
+                        "internalType": "string",
+                        "name": "circuitId",
+                        "type": "string"
+                    }
+                ],
+                "internalType": "struct ICircuitValidator.CircuitQuery",
+                "name": "query",
+                "type": "tuple"
+            }
+        ],
+        "name": "setZKPRequest",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint64",
+                "name": "requestId",
+                "type": "uint64"
+            },
+            {
+                "internalType": "uint256[]",
+                "name": "inputs",
+                "type": "uint256[]"
+            },
+            {
+                "internalType": "uint256[2]",
+                "name": "a",
+                "type": "uint256[2]"
+            },
+            {
+                "internalType": "uint256[2][2]",
+                "name": "b",
+                "type": "uint256[2][2]"
+            },
+            {
+                "internalType": "uint256[2]",
+                "name": "c",
+                "type": "uint256[2]"
+            }
+        ],
+        "name": "submitZKPResponse",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "supportedRequests",
+        "outputs": [
+            {
+                "internalType": "uint64",
+                "name": "",
+                "type": "uint64"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "newOwner",
+                "type": "address"
+            }
+        ],
+        "name": "transferOwnership",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+];
